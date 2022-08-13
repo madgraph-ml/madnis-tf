0bfbb5b6452f50084f61dc5dbf0fb5f2ab33cc6c
Ramon Winterhalder <rpw@mailbox.org>
2022-08-13 09:16:05 +0200

    Add normalized Camel on unit-hypercube

--- madnis/distributions/camel.py
+++ madnis/distributions/camel.py
@@ -147,7 +147,7 @@ class CuttedCamel(Distribution):
     def _prob(self, x, condition):
         # Note: the condition is ignored.
         del condition
-        
+
         prob = tf.zeros(x.shape[0], dtype=self._dtype)
         for i in range(self.npeaks):
             log_norm = tf.convert_to_tensor(
@@ -261,7 +261,7 @@ class MultiDimCamel(Distribution):
     def _prob(self, x, condition):
         # Note: the condition is ignored.
         del condition
-        
+
         prob = tf.zeros(x.shape[0], dtype=self._dtype)
         for i in range(self.npeaks):
             log_norm = self.log_norms[i]
@@ -272,6 +272,116 @@ class MultiDimCamel(Distribution):
                 tfutils.sum_except_batch(log_base + log_norm, num_batch_dims=1)
             )
         return prob
+
+    def _sample(self, num_samples, condition):
+        if condition is None:
+            rand_draw = np.random.binomial(num_samples, self.ratios[1:])
+            n0 = num_samples - np.sum(rand_draw)
+            n_samples = [n0] + [*rand_draw]
+            samples = [
+                self.means[i]
+                + tf.math.exp(self.log_stds[i])
+                * tf.random.normal((n_samples[i], *self._shape), dtype=self._dtype)
+                for i in range(self.npeaks)
+            ]
+            return tf.concat(samples, axis=0)
+        else:
+            # The value of the context is ignored, only its size is taken into account.
+            condition_size = condition.shape[0]
+            rand_draw = np.random.binomial(
+                condition_size * num_samples, self.ratios[1:]
+            )
+            n0 = condition_size * num_samples - np.sum(rand_draw)
+            n_samples = [n0] + [*rand_draw]
+            samples = [
+                self.means[i]
+                + tf.math.exp(self.log_stds[i])
+                * tf.random.normal((n_samples[i], *self._shape), dtype=self._dtype)
+                for i in range(self.npeaks)
+            ]
+            samples = tf.concat(samples, axis=0)
+            return tfutils.split_leading_dim(samples, [condition_size, num_samples])
+
+
+class NormalizedMultiDimCamel(Distribution):
+    """n-dimensional Camel distribution with m-peaks
+    which is normalized to 1 on the unit-hypercube [0,1]^n
+    """
+
+    def __init__(
+        self,
+        means: List[tf.Tensor],
+        stds: List[float],
+        dims: int,
+        peak_ratios: List[float] = None,
+        **kwargs
+    ):
+        """
+        Args:
+            means (List[tf.Tensor]): peak locations.
+            stds (List[float]): standard deviations.
+            dims (int): dimensionality of the distributions
+            peak_ratios (List[float], optional): relative peak contribution. Defaults to None.
+        """
+        super().__init__(**kwargs)
+        self._shape = tf.TensorShape([dims])
+
+        # check that its a proper tensor
+        if not all(isinstance(mean, tf.Tensor) for mean in means):
+            raise NotImplementedError("Means are not proper tensors")
+
+        # check that its floats
+        if any(isinstance(std, int) for std in stds):
+            stds = [float(i) for i in stds]
+
+        self.means = means
+        self.stds = tf.constant(stds, dtype=self._dtype)
+        self.log_stds = tf.constant(np.log(stds), dtype=self._dtype)
+
+        self.npeaks = len(means)
+
+        if peak_ratios is None:
+            fracs = 1 / self.npeaks
+            self.ratios = tf.constant([fracs] * self.npeaks, dtype=self._dtype)
+        else:
+            if len(peak_ratios) != self.npeaks:
+                raise ValueError("Length of lists do not match.")
+            self.ratios = tf.constant(
+                peak_ratios / tf.reduce_sum(peak_ratios), dtype=self._dtype
+            )
+
+        # Define the norm for all peaks
+        self.log_norms = tf.constant(
+            -1 / 2 * np.log(2 * np.pi) - self.log_stds, dtype=self._dtype
+        )
+
+    def _norm(self):
+        dims = self._shape[0]
+        norms = tf.pow(
+            0.5
+            * (
+                tf.math.erf((1 - self.means) / tf.sqrt(2) / self.stds)
+                + tf.math.erf((self.means) / tf.sqrt(2) / self.stds)
+            ),
+            dims,
+        )
+        return tf.reduce_sum(self.ratios * norms)
+
+    def _prob(self, x, condition):
+        # Note: the condition is ignored.
+        del condition
+
+        prob = tf.zeros(x.shape[0], dtype=self._dtype)
+        for i in range(self.npeaks):
+            log_norm = self.log_norms[i]
+            log_base = (
+                -0.5 * tf.math.exp(-2 * self.log_stds[i]) * ((x - self.means[i]) ** 2)
+            )
+            prob += self.ratios[i] * tf.math.exp(
+                tfutils.sum_except_batch(log_base + log_norm, num_batch_dims=1)
+            )
+        norm = self._norm()
+        return prob / norm
 
     def _sample(self, num_samples, condition):
         if condition is None:
