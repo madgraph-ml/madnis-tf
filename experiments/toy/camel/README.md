29fca8406b1035b9be6c19583999696762184832
Ramon Winterhalder <rpw@mailbox.org>
2022-08-22 10:52:11 +0200

    Add parts of first example

---
+++ experiments/toy/camel/README.md
@@ -0,0 +1,35 @@
+# 1-Dimensional Camel
+
+In this simple one-dimensional example we use 2 channel of
+integrations in which we use a cauchy distribution to map out the two
+gaussian peaks. We train a neural network to learn the optimal
+multi-channel weight $\alpha_i(x)$. This can be done in two ways.
+Either, the weight is trained from scratch or the training starts from
+a prior weight $\beta_i(x)$ which obeys the normalization
+
+```math
+\sum_i \beta_i(x) = 1, \qquad 0<beta_i(x) >1,
+```
+
+and thus only the some residual weight $\Delta_i(x,\theta)$ is learned. Requiring the normalization
+
+```math
+\sum_i \alpha_i(x\theta) = 1, \qquad 0<\alpha_i(x\theta) <1,
+```
+by applying a softmax activation in the last layer the full weight is then given by:
+
+```math
+\alpha_i(x\theta) = \frac{\beta_i(x)\cdot e^{\omega_i\cdot\Delta_i(x,\theta)}}{\sum_j \beta_j(x)\cdot e^{\omega_j\cdot\Delta_j(x,\theta)}},\qquad \text{with}\quad \omega_i^(0) = 0.
+```
+
+
+## Training
+
+Commands for ```camel```:
+
+```python
+# train the flow (optinally adding arguments, see --help)
+python train_camel.py (--arg ARG)
+```
+   
+   
---
+++ experiments/toy/camel/mcw.py
@@ -0,0 +1,42 @@
+import tensorflow as tf
+
+from madnis.nn.nets.mlp import MLP
+from madnis.nn.layers.residual import ResidualWeight
+from madnis.nn.layers.normalize import NormalizeWeights
+
+
+def MultiChannelWeight(
+    dims_in: int,
+    n_channels: int,
+    meta: dict,
+    name: str = "MCW",
+    **kwargs,
+):
+    """Defines a network to fit multi channel weights"""
+
+    x_input = tf.keras.Input((dims_in,))
+    x = MLP(meta, dims_in, n_channels)(x_input)
+    x_out = NormalizeWeights(use_probs=True)(x)
+
+    return tf.keras.Model(inputs=[x_input], outputs=x_out, name=name)
+
+
+def ResidualMultiChannelWeight(
+    dims_in: int,
+    n_channels: int,
+    meta: dict,
+    name: str = "ResidualMCW",
+    **kwargs,
+):
+    """Defines a network to fit multi channel weights
+    with residual inputs of some prior assumptions to make
+    the training easier.
+    """
+
+    x_input = tf.keras.Input((dims_in,))
+    residual = tf.keras.Input((n_channels,))
+    x = MLP(meta, dims_in, n_channels)(x_input)
+    x = ResidualWeight(n_channels)(x, residual)
+    x_out = NormalizeWeights(use_probs=True)(x)
+
+    return tf.keras.Model(inputs=[x_input, residual], outputs=x_out, name=name)
