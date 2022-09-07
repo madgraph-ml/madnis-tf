289dfd96d92f4941abfb491a55e59a2ee0a303fe
Theo Heimel <info@theovention.de>
2022-09-07 15:25:19 +0200

    two stage training bugfix

--- madnis/models/mc_integrator.py
+++ madnis/models/mc_integrator.py
@@ -427,7 +427,7 @@ class MultiChannelIntegrator:
                 tf.data.Dataset.from_tensor_slices(
                     (samples, q_sample, func_vals, channels)
                 )
-                .shuffle(tf.shape(samples)[0])
+                .shuffle(tf.shape(samples, out_type=tf.dtypes.int64)[0])
                 .batch(batch_size, drop_remainder=True)
             )
 
