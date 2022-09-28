e203236c1d4207a67a97e4d5a68efd3440a19935
Ramon Winterhalder <rpw@mailbox.org>
2022-09-28 16:52:38 +0200

    Update lhc examples

--- experiments/lhc/drell-yan/dy_integrand.py
+++ experiments/lhc/drell-yan/dy_integrand.py
@@ -1,53 +1,58 @@
-from zlib import MAX_WBITS
 import tensorflow as tf
 import math as m
+from pdfflow import mkPDFs
+import warnings
+warnings.filterwarnings("ignore")
 
+# -------------------------------------
 # Basic Inputs
-MW = 8.038500e+01
-MZ = 9.110000e+01
-WW = 2.047600e+00
-WZ = 2.441404e+00
-GF = 1.16637e-05
-NC = 3
+# -------------------------------------
+MW = 8.038500e+01   # W-Boson Mass
+MZ = 9.110000e+01   # Z-Boson Mass
+WZ = 2.441404e+00   # Z-Boson Width
+GF = 1.16637e-05    # Fermi Constant
+NC = 3              # Color factor
+# -------------------------------------
 
-class SquaredAmplitude():
-    """Squared amplitude for
+class DrellYan():
+    """Amplitude, weight and differential cross section
+    for the Drell-Yan process
     
-        e+ e- > Z/gamma > q q~
+        q q~ > Z/gamma > l+ l-
         
-    where q = {u,c,d,s}
+    where q = {u,c,d,s}.
     """
     def __init__(
         self,
-        fsq: str,
+        isq: str,
         mw: float = MW,
         mz: float = MZ,
-        ww: float = WW,
         wz: float = WZ,
         gf: float = GF,
         **kwargs
     ):
         """
         Args:
-            fsq (str): Whether the final state quark is up-quark type or down-quark type.
+            fsq (str): Whether the initial state quark is up-quark type or down-quark type.
                 up-quark type   : `"u"`, `"c"`,
                 down-quark type : `"d"`, `"s"`
         """
         super().__init__(**kwargs)
-        self.fsq = fsq
+        self.isq = isq
         self._dtype = tf.keras.backend.floatx()
         
         # Input parameters
         self.mw = mw
         self.mz = mz
-        self.ww = ww
         self.wz = wz
         self.gf = gf
         
         # Basic Definitions
         self.cw2 = self.mw**2/self.mz**2
         self.sw2 = 1 - self.cw2
-        self.alpha = 1/1.279000e+02
+        
+        # Define alpha in G_mu scheme (alpha_0 would be 1/1.279000e+02)
+        self.alpha = m.sqrt(2) * self.gf * self.mw **2 * (1 - self.cw2)
 
         self.V_l = -1/2 + 2 * self.sw2
         self.A_l = -1/2
@@ -71,13 +76,13 @@ class SquaredAmplitude():
             return -1/2
         
     def chi_1(self, s: float):
-        factor = tf.math.sqrt(2) * self.gf * self.mz**2/(16 * m.pi * self.alpha)
+        factor = m.sqrt(2) * self.gf * self.mz**2/(16 * m.pi * self.alpha)
         num = s * (s - self.mz**2)
         den = (s - self.mz**2)**2 + self.wz**2 * self.mz**2
         return factor * num/den
 
     def chi_2(self, s: float):
-        factor = tf.math.sqrt(2) * self.gf * self.mz**2/(16 * m.pi * self.alpha)
+        factor = m.sqrt(2) * self.gf * self.mz**2/(16 * m.pi * self.alpha)
         num = s**2
         den = (s - self.mz**2)**2 + self.wz**2 * self.mz**2
         return factor**2 * num/den
--- experiments/lhc/lepton/README.md
+++
@@ -1,9 +0,0 @@
-# Lepton collider
-
-We start by considering the following simple process
-
-$$
-\mathrm{e}^{-} \mathrm{e}^{+}\to \mathrm{Z}/\gamma \to \mathrm{q} \bar{\mathrm{q}},
-$$
-
-and provide the corresponding full amplitude by a tensorflow function. This makes this integrand differentiable and allows for optimized training methods.
