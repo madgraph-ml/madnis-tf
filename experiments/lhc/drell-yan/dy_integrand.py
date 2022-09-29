08f62e90fa8e9dc1a3b81d91c27952b88a44c176
Ramon Winterhalder <rpw@mailbox.org>
2022-09-29 14:51:02 +0200

    Update DY integrand

--- experiments/lhc/drell-yan/dy_integrand.py
+++ experiments/lhc/drell-yan/dy_integrand.py
@@ -1,7 +1,9 @@
-import tensorflow as tf
+from typing import List
 import math as m
-from pdfflow import mkPDFs
 import warnings
+
+import tensorflow as tf
+from pdfflow import mkPDFs
 
 warnings.filterwarnings("ignore")
 
@@ -17,17 +19,17 @@ NC = 3  # Color factor
 
 
 class DrellYan:
-    """Amplitude, weight and differential cross section
+    r"""Amplitudes, weights and differential cross section
     for the Drell-Yan process
 
         q q~ > Z/gamma > l+ l-
 
-    where q = {u,c,d,s}.
+    where q = {u,c,b,d,s}.
     """
 
     def __init__(
         self,
-        isq: str,
+        isq: List[str],
         e_had: float = 13000.0,
         mw: float = MW,
         mz: float = MZ,
@@ -38,9 +40,16 @@ class DrellYan:
     ):
         """
         Args:
-            isq (str): Whether the initial state quark is up-quark type or down-quark type.
+            isq (List[str]): List of initial state quarks. Possible are:
                 up-quark type   : `"u"`, `"c"`,
-                down-quark type : `"d"`, `"s"`
+                down-quark type : `"d"`, `"s"`, `"b"`
+            e_had (float, optional): Center-of-mass energy in LAB frame. Defaults to 13000.0.
+            mw (float, optional): W-boson mass. Defaults to MW.
+            mz (float, optional): Z-boson mass. Defaults to MZ.
+            wz (float, optional): Z-boson width. Defaults to WZ.
+            gf (float, optional): Fermi constant. Defaults to GF.
+            pdfset (str, optional): PDF-set to use.. Defaults to "NNPDF40_lo_as_01180".
+                Requires `lhapdf` and `lhapdf-management`.
         """
         super().__init__(**kwargs)
         self._dtype = tf.keras.backend.floatx()
@@ -62,77 +71,187 @@ class DrellYan:
         self.sw2 = 1 - self.cw2
 
         # Define alpha in G_mu scheme (alpha_0 would be 1/1.279000e+02)
-        self.alpha = m.sqrt(2) * self.gf * self.mw**2 * self.sw
+        self.alpha = m.sqrt(2) * self.gf * self.mw**2 * self.sw2
 
-        # Define pid and initial state quark
+        # Define list of initial state quarks
         self.isq = isq
-        pid = {"d": 1, "u": 2, "s": 3, "c": 4}[self.isq]
-        self.pid = tf.cast([pid], dtype=tf.int32)
+        assert isinstance(self.isq, list)
 
-        # Define quark charges
-        self.Q_f = {"d": -1 / 3, "s": -1 / 3, "u": 2 / 3, "c": 2 / 3}[self.isq]
-
-        # Define couplings
-
+        # Define couplings and charges
         self.V_l = -1 / 2 + 2 * self.sw2
         self.A_l = -1 / 2
+
+        self.Q_f = {
+            "d": -1 / 3,
+            "s": -1 / 3,
+            "u": 2 / 3,
+            "c": 2 / 3,
+            "b": 2 / 3,
+        }
 
         self.V_q = {
             "d": -1 / 2 + 2 / 3 * self.sw2,
             "s": -1 / 2 + 2 / 3 * self.sw2,
             "u": +1 / 2 - 4 / 3 * self.sw2,
             "c": +1 / 2 - 4 / 3 * self.sw2,
-        }[self.isq]
+            "b": +1 / 2 - 4 / 3 * self.sw2,
+        }
 
-        self.A_q = {"d": -1 / 2, "s": -1 / 2, "u": 1 / 2, "c": 1 / 2}[self.isq]
+        self.A_q = {
+            "d": -1 / 2,
+            "s": -1 / 2,
+            "u": 1 / 2,
+            "c": 1 / 2,
+            "b": 1 / 2,
+        }
 
-    def chi_1(self, s: float):
-        factor = m.sqrt(2) * self.gf * self.mz**2 / (16 * m.pi * self.alpha)
-        num = s * (s - self.mz**2)
-        den = (s - self.mz**2) ** 2 + self.wz**2 * self.mz**2
-        return factor * num / den
+    def a(self, s: tf.Tensor, mode: str, isq: str):
+        if mode == "ZZ":
+            m_ZZ = (
+                (self.V_q[isq] ** 2 + self.A_q[isq] ** 2)
+                * (self.V_l**2 + self.A_l**2)
+                / 4
+            )
+            factor_g = 8 / m.sqrt(2) * self.gf * self.mz**2
+            return s**2 / 4 * factor_g**2 * m_ZZ
+        elif mode == "yy":
+            m_yy = 4 * self.Q_f[isq] ** 2
+            factor_e = 4 * m.pi * self.alpha
+            return s**2 / 4 * factor_e**2 * m_yy
+        else:
+            m_yz = self.Q_f[isq] * self.V_q[isq] * self.V_l
+            factor_e = 4 * m.pi * self.alpha
+            factor_g = 8 / m.sqrt(2) * self.gf * self.mz**2
+            return s**2 / 4 * factor_g * factor_e * m_yz
 
-    def chi_2(self, s: float):
-        factor = m.sqrt(2) * self.gf * self.mz**2 / (16 * m.pi * self.alpha)
-        num = s**2
-        den = (s - self.mz**2) ** 2 + self.wz**2 * self.mz**2
-        return factor**2 * num / den
+    def b(self, s: tf.Tensor, mode: str, isq: str):
+        if mode == "ZZ":
+            m_ZZ = self.A_q[isq] * self.A_l * self.V_q[isq] * self.V_l
+            factor_g = 8 / m.sqrt(2) * self.gf * self.mz**2
+            return s**2 / 2 * factor_g**2 * m_ZZ
+        elif mode == "yy":
+            return 0.0
+        else:
+            m_yz = self.Q_f[isq] * self.A_q[isq] * self.A_l
+            factor_e = 4 * m.pi * self.alpha
+            factor_g = 8 / m.sqrt(2) * self.gf * self.mz**2
+            return s**2 / 2 * factor_g * factor_e * m_yz
 
-    def m0(self, s: float):
-        m_yy = self.Q_f**2
-        m_yZ = -2 * self.Q_f * self.V_l * self.V_q * self.chi_1(s)
-        m_ZZ = (
-            (self.V_q**2 + self.A_q**2)
-            * (self.V_l**2 + self.A_l**2)
-            * self.chi_2(s)
-        )
-        return m_yy + m_yZ + m_ZZ
+    def prop_factor(self, s: tf.Tensor, mode: str):
+        if mode == "ZZ":
+            den = (s - self.mz**2) ** 2 + self.wz**2 * self.mz**2
+            return 1 / den
+        elif mode == "yy":
+            den = s**2
+            return 1 / den
+        else:
+            nom = s - self.mz**2
+            den = s * ((s - self.mz**2) ** 2 + self.wz**2 * self.mz**2)
+            return nom / den
 
-    def m1(self, s: float):
-        m_yZ = -4 * self.chi_1(s) * self.Q_f * self.A_q * self.A_l
-        m_ZZ = +8 * self.chi_2(s) * self.A_q * self.A_l * self.V_q * self.V_l
-        return m_yZ + m_ZZ
+    def amp2_single(self, cos_theta: tf.Tensor, s: tf.Tensor, mode: str, isq: str):
+        """Squared single diagram matrix element for a given production mode
 
-    def amp2(self, cos_theta: float, s: float):
-        return (
-            16
-            * m.pi**2
-            * self.alpha**2
-            * (self.m0(s) * (1 + cos_theta**2) + self.m1(s) * cos_theta)
-        )
+        Args:
+            cos_theta (tf.Tensor): scattering angle in partonic CM frame.
+            s (tf.Tensor): partonic CM energy.
+            mode (str): Which diagram: `"ZZ"`, `"yy"`,`"yZ"`
+            isq (str): initial-state quark.
 
-    def partonic_weight(self, cos_theta: float, s: float):
-        factor = 16 * m.pi**2 * 4 * NC * s
-        return self.amp2(cos_theta, s) / factor
+        Returns:
+            m2: Squared single diagram matrix element (|M_Z|^2 or |M_y|^2),
+                or Re(M_yM^*_Z) for interference depending on the mode.
+        """
+        if mode == "yy":
+            return (
+                self.prop_factor(s, mode) * self.a(s, mode, isq) * (1 + cos_theta**2)
+            )
 
-    def hadronic_weight(self, x1: tf.Tensor, x2: tf.Tensor, cos_theta: tf.Tensor):
+        m_sym = self.a(s, mode, isq) * (1 + cos_theta**2)
+        m_asym = self.b(s, mode, isq) * cos_theta
+        return self.prop_factor(s, mode) * (m_sym + m_asym)
+
+    def amp2_all(self, cos_theta: tf.Tensor, s: tf.Tensor, isq: str):
+        """Full squared matrix element. Note the relative sign
+        of the interference contribution.
+
+        Args:
+            cos_theta (tf.Tensor): scattering angle in partonic CM frame.
+            s (tf.Tensor): partonic CM energy.
+            isq (str): initial-state quark.
+
+        Returns:
+            m2: Full squared matrix element (|M_Z + M_y|^2)
+        """
+        m_yy = self.amp2_single(cos_theta, s, "yy", isq)
+        m_ZZ = self.amp2_single(cos_theta, s, "ZZ", isq)
+        m_int = self.amp2_single(cos_theta, s, "yZ", isq)
+        return m_yy + m_ZZ - 2 * m_int
+
+    def partonic_dxs(self, cos_theta: tf.Tensor, s: tf.Tensor, isq: str):
+        """Fully differential partonic cross section, i.e.
+
+            dsigma/dOmega = 1/(16 PI^2) * 1/s * 1/(4 * NC) * |M|^2
+
+        which includes:
+            - ps-weight: 1/(16 PI^2) * 1/s
+            - averaging/summation over initial/final-state spins: 1/4
+            - averaging over initial-state color: 1/NC
+
+        Args:
+            cos_theta (tf.Tensor): scattering angle in partonic CM frame.
+            s (tf.Tensor): partonic CM energy.
+            isq (str): initial-state quark.
+
+        Returns:
+            p_dxs (tf.Tensor): partonic differential cross-section
+        """
+        cs_factor = 1 / (4 * NC)
+        ps_weight = 1 / (16 * m.pi**2 * s)
+        return ps_weight * cs_factor * self.amp2_all(cos_theta, s, isq)
+
+    def hadronic_dxs(
+        self,
+        x1: tf.Tensor,
+        x2: tf.Tensor,
+        cos_theta: tf.Tensor,
+        isq: str,
+    ):
+        """Fully differential hadronic cross section, including the
+        parton density functions (PDFs).
+
+        Args:
+            x1 (tf.Tensor): momentum fraction x1.
+            x2 (tf.Tensor): momentum fraction x2.
+            cos_theta (tf.Tensor): scattering angle in partonic CM frame.
+            isq (str): initial-state quark.
+
+        Returns:
+            h_dxs (tf.Tensor): hadronic differential cross-section
+        """
+        # Get Particle ID
+        pid = {"d": 1, "u": 2, "s": 3, "c": 4, "b": 5}[isq]
+        pid = tf.cast([pid], dtype=tf.int32)
+
+        # Calculate pdfs
         q2 = tf.ones_like(x1) * self.muf
-        pdf_1 = self.pdf.xfxQ2(self.pid, x1, q2) / x1
-        pdf_2 = self.pdf.xfxQ2(-self.pid, x2, q2) / x2
-        return pdf_1 * pdf_2 * self.partonic_weight(cos_theta, x1 * x2 * self.s_had)
+        pdf_1 = self.pdf.xfxQ2(pid, x1, q2) / x1
+        pdf_2 = self.pdf.xfxQ2(-pid, x2, q2) / x2
+        s_parton = x1 * x2 * self.s_had
+        return pdf_1 * pdf_2 * self.partonic_dxs(cos_theta, s_parton, isq)
 
-    def call(self, p):
-        # Current
+    def __call__(self, p: tf.Tensor):
+        """Calculate the full hadronic event weight including pdfs and
+        sum over initial-state flavors.
+
+        Args:
+            p (tf.Tensor): Momentum input with shape `(N,4)` and parametrization
+                `p = {px1, py1, pz1, pz2}`
+
+        Returns:
+            w (tf.Tensor): Returns weight of the event with shape `(1,)`.
+        """
+        # Map input to needed quantities
         px1, py1, pz1, pz2 = tf.unstack(p, axis=-1)
         e1 = tf.math.sqrt(px1**2 + py1**2 + pz1**2)
         e2 = tf.math.sqrt(px1**2 + py1**2 + pz2**2)
@@ -143,8 +262,9 @@ class DrellYan:
         x1 = (e_tot + pz_tot) / (self.e_had)
         x2 = (e_tot - pz_tot) / (self.e_had)
 
-        # alternative?
-        # x1, x2, cos_theta, phi = tf.unstack(p, axis=-1)
+        # Calculat full weight
+        w = 0
+        for isq in self.isq:
+            w += self.hadronic_dxs(x1, x2, cos_theta, isq)
 
-        w = self.hadronic_weight(x1, x2, cos_theta)
-        raise w
+        return w
