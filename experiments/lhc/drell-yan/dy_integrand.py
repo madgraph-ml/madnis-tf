Fix single precision error

experiments/lhc/drell-yan/dy_integrand.py
@@ -241,7 +241,9 @@ class DrellYan:
         pid = tf.cast([pid], dtype=tf.int32)
 
         # Calculate pdfs
-        q2 = tf.ones_like(x1) * self.muf
+        q2 = tf.cast(tf.ones_like(x1) * self.muf, dtype=self._dtype)
+        x1 = tf.cast(x1, dtype=self._dtype)
+        x2 = tf.cast(x2, dtype=self._dtype)
         pdf_1 = self.pdf.xfxQ2(pid, x1, q2) / x1
         pdf_2 = self.pdf.xfxQ2(-pid, x2, q2) / x2
         s_parton = x1 * x2 * self.s_had
@@ -270,8 +272,14 @@ class DrellYan:
             x1 = (e_tot + pz_tot) / (self.e_had)
             x2 = (e_tot - pz_tot) / (self.e_had)
 
+            # s = self.s_had * x1 * x2
+            # r2 = tf.math.log(x1) / tf.math.log(s / self.s_had)
+            # r3 = (2*pz1/self.e_had + x2) / (x1 + x2)
+            # det1 = 4 * m.pi * tf.math.log(self.s_had/s) / self.s_had
+            # det2 = m.pi * (s/self.s_had)**(-2 * r2) * (-r3*s + (-1 + r3) * (s/self.s_had)**(2 * r2) * self.s_had) * (s - r3 * s + r3 * (s/self.s_had)**(2 * r2) * self.s_had) * tf.math.log(s/self.s_had) / (4 * self.s_had)
+
             # Trafo determinant
-            det = 1.0
+            det = 1.0 #det1**2/det2
 
         elif self.input_format == "convpolar":
             # Map input to needed quantities
@@ -287,4 +295,4 @@ class DrellYan:
         for isq in self.isq:
             w += self.hadronic_dxs(x1, x2, cos_theta, isq)
 
-        return det * w
+        return w * det

madnis/mappings/phasespace_2p.py
@@ -21,9 +21,9 @@ class TwoParticlePhasespaceA(Mapping):
         super().__init__(StandardUniform([4]), **kwargs)
         self._shape = tf.TensorShape([4])
 
-        self.e_beam = e_beam
-        self.s_min = sqrt_s_min**2
-        self.s_max = 4*e_beam**2
+        self.e_beam = tf.constant(e_beam, dtype=self._dtype)
+        self.s_min = tf.constant(sqrt_s_min**2, dtype=self._dtype)
+        self.s_max = tf.constant(4*e_beam**2, dtype=self._dtype)
         self.tf_pi = tf.constant(np.pi, dtype=self._dtype)
 
         self.massless = s_mass == 0.
@@ -32,8 +32,8 @@ class TwoParticlePhasespaceA(Mapping):
         else:
             self.y1 = tf.math.atan((self.s_min - s_mass**2) / (s_mass*s_gamma))
             self.y2 = tf.math.atan((self.s_max - s_mass**2) / (s_mass*s_gamma))
-            self.s_mass = s_mass
-            self.s_gamma = s_gamma
+            self.s_mass = tf.constant(s_mass, dtype=self._dtype)
+            self.s_gamma = tf.constant(s_gamma, dtype=self._dtype)
 
     def _sublogdet(self, s, r2, r3, r4):
         del r4
@@ -118,7 +118,6 @@ class TwoParticlePhasespaceA(Mapping):
         phi = 2 * self.tf_pi * (r4 - 0.5)
         px1 = pt * tf.math.cos(phi)
         py1 = pt * tf.math.sin(phi)
-
         p = tf.stack((px1, py1, pz1, pz2), axis=-1)
         return p, logdet + self._sublogdet(s, r2, r3, r4)
 
@@ -152,9 +151,9 @@ class TwoParticlePhasespaceB(Mapping):
         super().__init__(StandardUniform([4]), **kwargs)
         self._shape = tf.TensorShape([4])
 
-        self.e_beam = e_beam
-        self.s_min = sqrt_s_min**2
-        self.s_max = 4*e_beam**2
+        self.e_beam = tf.constant(e_beam, dtype=self._dtype)
+        self.s_min = tf.constant(sqrt_s_min**2, dtype=self._dtype)
+        self.s_max = tf.constant(4*e_beam**2, dtype=self._dtype)
         self.tf_pi = tf.constant(np.pi, dtype=self._dtype)
 
         self.massless = s_mass == 0.
@@ -163,8 +162,8 @@ class TwoParticlePhasespaceB(Mapping):
         else:
             self.y1 = tf.math.atan((self.s_min - s_mass**2) / (s_mass*s_gamma))
             self.y2 = tf.math.atan((self.s_max - s_mass**2) / (s_mass*s_gamma))
-            self.s_mass = s_mass
-            self.s_gamma = s_gamma
+            self.s_mass = tf.constant(s_mass, dtype=self._dtype)
+            self.s_gamma = tf.constant(s_gamma, dtype=self._dtype)
 
     def _sublogdet(self, s):
         det = 4 * self.tf_pi * tf.math.log(self.s_max/s) / self.s_max