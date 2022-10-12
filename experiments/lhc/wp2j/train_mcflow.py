a60bad5172b75e59916fe7ce532cacd4691eb2e3
Ramon Winterhalder <rpw@mailbox.org>
2022-10-12 17:39:02 +0200

    Update symfact and remove alpha for now

--- experiments/lhc/wp2j/train_mcflow.py
+++ experiments/lhc/wp2j/train_mcflow.py
@@ -8,9 +8,11 @@ import time
 from mcw import mcw_model, residual_mcw_model
 from madnis.utils.train_utils import integrate
 from madnis.models.mc_integrator import MultiChannelIntegrator
+from madnis.distributions.uniform import StandardUniform
 from madnis.distributions.camel import NormalizedMultiDimCamel
 from madnis.nn.nets.mlp import MLP
 from vegasflow import VegasFlow, RQSVegasFlow
+from utils import translate_channels
 
 import sys
 
@@ -29,7 +31,7 @@ parser = argparse.ArgumentParser()
 
 # Data params
 parser.add_argument("--train_batches", type=int, default=1000)
-parser.add_argument("--int_samples", type=int, default=10000)
+parser.add_argument("--int_samples", type=int, default=1000000)
 
 # model params
 parser.add_argument("--use_prior_weights", action='store_true')
@@ -45,11 +47,11 @@ parser.add_argument("--mcw_units", type=int, default=16)
 parser.add_argument("--mcw_layers", type=int, default=2)
 
 # Define the number of channels
-parser.add_argument("--channels", type=int, default=2)
+parser.add_argument("--channels", type=int, default=4)
 
 # Train params
 parser.add_argument("--epochs", type=int, default=20)
-parser.add_argument("--batch_size", type=int, default=128)
+parser.add_argument("--batch_size", type=int, default=1000)
 parser.add_argument("--lr", type=float, default=1e-3)
 
 args = parser.parse_args()
@@ -59,8 +61,8 @@ args = parser.parse_args()
 ################################
 
 DTYPE = tf.keras.backend.floatx()
-DIMS_IN = 20  # dimensionality of data space (apparently it must be 12?)
-N_CHANNELS = 4  # number of Channels
+DIMS_IN = 12  # dimensionality of data space (apparently it must be 12?)
+N_CHANNELS = args.channels  # number of Channels
 
 #cwd = os.getcwd()
 os.chdir("MadNis_example")
@@ -69,8 +71,8 @@ madgraph = tf.load_op_library("SubProcesses/P1_gg_wpqq/madevent_tf.so")
 
 # 0,1,2,3 -> 0,2,3,6
 def integrand(x, channels):
-    #return madgraph.call_madgraph(x, tf.one_hot(channels, N_CHANNELS, dtype=tf.int32))
-    return madgraph.call_madgraph(x, channels)
+    channels_api = translate_channels(channels)
+    return madgraph.call_madgraph(x, channels_api)
 
 print(f"\n Integrand specifications:")
 print("-----------------------------------------------------------")
@@ -174,6 +176,8 @@ lr_schedule2 = tf.keras.optimizers.schedules.InverseTimeDecay(LR, DECAY_STEP, DE
 opt1 = tf.keras.optimizers.Adam(lr_schedule1)
 opt2 = tf.keras.optimizers.Adam(lr_schedule2)
 
+base_dist = StandardUniform((DIMS_IN,))
+
 integrator = MultiChannelIntegrator(
     integrand, flow, [opt1],
     mcw_model=None,
@@ -190,11 +194,11 @@ integrator = MultiChannelIntegrator(
 res, err = integrator.integrate(INT_SAMPLES, weight_prior=madgraph_prior)
 relerr = err / res * 100
 
-print(f"\n Pre Multi-Channel integration ({INT_SAMPLES:.1e} samples):")
-print("--------------------------------------------------------------")
-print(f" Number of channels: {N_CHANNELS}                            ")
-print(f" Result: {res:.8f} +- {err:.8f} ( Rel error: {relerr:.4f} %) ")
-print("------------------------------------------------------------\n")
+print(f"\n Pre Multi-Channel integration ({INT_SAMPLES:.1e} samples):  ")
+print("----------------------------------------------------------------")
+print(f" Number of channels: {N_CHANNELS}                              ")
+print(f" Result: {res:.8f} +- {err:.8f} pb ( Rel error: {relerr:.4f} %)")
+print("----------------------------------------------------------------\n")
 
 ################################
 # Train the network
@@ -232,8 +236,8 @@ print("--- Run time: %s secs ---" % ((end_time - start_time)))
 res, err = integrator.integrate(INT_SAMPLES, weight_prior=madgraph_prior)
 relerr = err / res * 100
 
-print(f"\n Opt. Multi-Channel integration ({INT_SAMPLES:.1e} samples):")
-print("---------------------------------------------------------------")
-print(f" Number of channels: {N_CHANNELS}                             ")
-print(f" Result: {res:.8f} +- {err:.8f} ( Rel error: {relerr:.4f} %)  ")
-print("-------------------------------------------------------------\n")
+print(f"\n Opt. Multi-Channel integration ({INT_SAMPLES:.1e} samples): ")
+print("----------------------------------------------------------------")
+print(f" Number of channels: {N_CHANNELS}                              ")
+print(f" Result: {res:.8f} +- {err:.8f} pb ( Rel error: {relerr:.4f} %)")
+print("----------------------------------------------------------------\n")
