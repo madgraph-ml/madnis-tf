ed498e03208bb863385a65b5668cd238fc8e8154
Claudius Krause <Claudius.Krause.21@gmail.com>
2022-10-24 15:08:11 +0200

    Fix issue with raise->return, some style changes

--- madnis/transforms/actnorm.py
+++ madnis/transforms/actnorm.py
@@ -1,4 +1,4 @@
-""" Random Permutation """
+""" ActNorm of 1807.03039 """
 
 from typing import Union
 import numpy as np
@@ -17,29 +17,29 @@ class ActNorm(Transform):
             Kingma & Dhariwal, 2018, https://arxiv.org/abs/1807.03039
     '''
 
-    def __init__(self, 
-        dims_in,
-        dims_c=None,
-        global_affine_init: float = 1.0,
-        global_affine_type: str = "SOFTPLUS",
-        eps: float = 1e-6,
-    ):
+    def __init__(self,
+                 dims_in,
+                 dims_c=None,
+                 global_affine_init: float = 1.0,
+                 global_affine_type: str = "SOFTPLUS",
+                 eps: float = 1e-6,
+                 ):
         """
         Args:
             global_affine_init (float): Initial value for the global affine scaling. Defaults to 1.
-            global_affine_type (str): ``'SIGMOID'``, ``'SOFTPLUS'``, or ``'EXP'``. 
-                Defines the activation to be used on the beta for the global 
+            global_affine_type (str): ``'SIGMOID'``, ``'SOFTPLUS'``, or ``'EXP'``.
+                Defines the activation to be used on the beta for the global
                 affine scaling. Defaults to ``'SOFTPLUS'``.
             eps (float, optional): Small epsilon value. Defaults to 1e-6.
         """
         super().__init__(dims_in, dims_c)
-        
+
         self.channels = self.dims_in[-1]
         # rank of the tensors means 1d, 2d, 3d tensor etc.
         self.input_rank = len(self.dims_in) - 1
         self.sum_dims = tuple(range(1, 2 + self.input_rank))
         self.eps = eps
-        
+
         if global_affine_type == "SIGMOID":
             global_scale = 2.0 - np.log(10.0 / global_affine_init - 1.0)
             self.global_scale_activation = lambda a: 10 * tf.sigmoid(a - 2.0)
@@ -55,7 +55,7 @@ class ActNorm(Transform):
             raise ValueError(
                 'Global affine activation must be "SIGMOID", "SOFTPLUS" or "EXP"'
             )
-            
+
         self.global_scale = self.add_weight(
             "global_scale",
             shape=(1, *([1] * self.input_rank), self.channels),
@@ -79,9 +79,10 @@ class ActNorm(Transform):
     def inverse(self, z, c=None, jac=True):
         scale = self.global_scale_activation(self.global_scale)
         x = (z - self.global_offset) / scale
-        ldj = (-1) * tf.reduce_sum((tf.math.log(scale)), axis=self.sum_dims) * self.ldj_multiplier(z)
+        ldj = (-1) * tf.reduce_sum((tf.math.log(scale)), axis=self.sum_dims) *\
+            self.ldj_multiplier(z)
         return x, ldj
 
     def ldj_multiplier(self, x):
         '''Multiplier for ldj'''
-        raise tf.size(x[0,...,:1], out_type=x.dtype)
+        return tf.size(x[0, ..., :1], out_type=x.dtype)
