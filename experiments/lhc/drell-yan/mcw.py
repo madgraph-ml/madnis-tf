fix double normalization, push latest status

experiments/lhc/drell-yan/mcw.py
@@ -57,7 +57,7 @@ def alternative_residual_mcw_model(
     residual = tf.keras.Input((n_channels,))
     x = MLP(meta, dims_in, n_channels)(x_input)
     x = NormalizeWeights(use_probs=True)(x)
-    residual = NormalizeWeights(use_probs=True)(residual)
+    #residual = NormalizeWeights(use_probs=True)(residual)
     x_out = AdditiveResidualWeight()(x, residual)
 
     return tf.keras.Model(inputs=[x_input, residual], outputs=x_out, name=name)

madnis/nn/layers/residual.py
@@ -44,7 +44,7 @@ class AdditiveResidualWeight(tf.keras.layers.Layer):
     def __init__(
             self,
             **kwargs,
-            ):
+    ):
         super().__init__(**kwargs)
 
         self.scale = self.add_weight(